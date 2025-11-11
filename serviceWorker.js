1be08d29feedab490a46de3b7de61c144493274a
Roge Reksuby <e.kulygin2018@gmail.com>
2025-11-12 00:10:47 +0300

    Updates

---
+++ serviceWorker.js
@@ -0,0 +1,9 @@
+// public/serviceWorker.js
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', (event) => {
+  
+  console.log("fetch");
+});
